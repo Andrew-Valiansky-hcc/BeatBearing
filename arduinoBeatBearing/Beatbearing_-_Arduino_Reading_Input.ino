c3fa46559490f7d7b014dec2897b9caf435134f1
Andrew-Valiansky-hcc <44903662+Andrew-Valiansky-hcc@users.noreply.github.com>
2018-11-12 15:55:36 -0500

    Adding starting point for reading input from board

---
+++ arduinoBeatBearing/Beatbearing_-_Arduino_Reading_Input.ino
@@ -0,0 +1,52 @@
+int stateMatrix[32][8] = {        
+  0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0};  
+
+boolean bState[32] = {
+  B0, B0, B0, B0, B0, B0, B0, B0,
+  B0, B0, B0, B0, B0, B0, B0, B0,
+  B0, B0, B0, B0, B0, B0, B0, B0,
+  B0, B0, B0, B0, B0, B0, B0, B0};
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(A0, INPUT);                        
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);      
+  pinMode(A5, INPUT);
+  pinMode(A6, INPUT);
+  pinMode(A7, INPUT);
+   
+}
+
+void loop() 
+{
+  Serial.println(A2); 
+
+
+  int state = 1;
+  for (int n = 0; n < 32; n++) 
+  {
+    if (state = 0)
+    {
+    ("Empty");
+
+    }
+    
+    else
+    { 
+    ("Complete");
+
+    }
+    
+  }
+}
+
+
+
