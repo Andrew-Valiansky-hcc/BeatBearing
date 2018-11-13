88d3be3ab9f0e106fc46ae53cc6d8b334f9c4815
1sfoerster <1sfoerster@users.noreply.github.com>
2018-11-13 15:32:00 -0500

    added some comments

--- pythonBeatBearing/drumTest.py
+++ pythonBeatBearing/drumTest.py
@@ -1,3 +1,9 @@
+# Executing drumTest.py depends upon the python environment being able to find these three files:
+#	ctcsound.py
+#	drumMachine.orc
+#	drumbTest.sco
+# These three files (and some jupyte files explaining things) are found in the file ctcsound.zip
+
 import ctcsound
 cs = ctcsound.Csound()
 ret = cs.compile_("csound", "-o", "dac", "C:/Users/SET165-07U/Downloads/drumMachine.orc", "C:/Users/SET165-07U/Downloads/drumTest.sco")
