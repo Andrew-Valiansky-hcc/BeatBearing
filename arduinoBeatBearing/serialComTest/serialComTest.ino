2ecfb956e24577060d18ede5bd68e0d8f183f0c5
1sfoerster <1sfoerster@users.noreply.github.com>
2018-11-27 08:06:05 -0500

    bit level control over what arduino sends
    arduino com port monitor and putty agree ... sends 251-68 with lf and cr over and over again

--- arduinoBeatBearing/serialComTest/serialComTest.ino
+++ arduinoBeatBearing/serialComTest/serialComTest.ino
@@ -1,20 +1,29 @@
-//says "Goodnight moon!" over and over again
-//putty and arduino serial monitor can not be listening simultaneously
-
 #include <SoftwareSerial.h>
-
 SoftwareSerial mySerial(10, 11); // RX, TX
+byte pinStateL;
+byte pinStateH;
 
 void setup() {
+  
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(19200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  Serial.println("Goodnight moon!");
+  pinStateL = B01000000;
+  bitSet(pinStateL, 2); //pinState = B01000100 which is 68 decimal
+ 
+  pinStateH = B11111111;
+  bitClear(pinStateH, 2); //pinState = B11111011 which is 251 decimal
+
 }
 
 void loop() { // run over and over
-  Serial.println("Goodnight moon!");
+  int i;
+  Serial.print(pinStateH);
+  Serial.print("-");
+  Serial.print(pinStateL);
+  Serial.println();  
+  delay(500);
 }
