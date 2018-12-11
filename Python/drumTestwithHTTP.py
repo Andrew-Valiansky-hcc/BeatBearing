0fc82979878b8928374e25634a55d63881b309d2
1sfoerster <1sfoerster@users.noreply.github.com>
2018-12-11 09:34:48 -0500

    folderNameChanges

---
+++ Python/drumTestwithHTTP.py
@@ -0,0 +1,35 @@
+# this program has the same requirements as drumTest
+# to execute it, in the canopy editor, go to Run then press reset
+# hit the green arrow to start it listening for processing
+# then start the drumTest processing sketch
+# you will hear the drumTest ctcsound output until you kill the processing sketch
+
+import ctcsound
+cs = ctcsound.Csound()
+
+# web server
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    print('..',data,'..') # Paging Python!
+    # do whatever you need to do with the data
+    print(" b'start' ")
+    print('here')
+    ret = cs.compile_("csound", "-o", "dac", "C:/Users/Scott Foerster/Downloads/ctcsoundmaster/cookbook/examples/drumMachine.orc", "C:/Users/Scott Foerster/Downloads/ctcsoundmaster/cookbook/examples/drumTest.sco")
+    if ret == ctcsound.CSOUND_SUCCESS:
+        cs.perform()
+        cs.reset()
+        break
+conn.close()
+# optionally put a loop here so that you start 
+# listening again after the connection closes
+
